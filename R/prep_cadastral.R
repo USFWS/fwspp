50cc86c89959811215e46def3d33120e2b9c51ba
Adam Smith <avianmigration@gmail.com>
2017-10-16 15:26:34 +0000

    Now used to convert `gdb` to `sf` for storage
    * instead of loading and filtering `gdb` every run
    * improves property loading speed
    * closes #4
    * loading properties for queries moved to new fxn (`prep_properties`) in subsequent commit

--- R/prep_cadastral.R
+++ R/prep_cadastral.R
@@ -1,60 +1,51 @@
 #' Prepare USFWS cadastral spatial data
 #'
-#' Converts USFWS cadastral geodatabase into \code{\link[sf]{sf}} and
-#'  filters to relevant properties, if necessary.  Exported by not
-#'  typically called directly by user.
+#' Converts USFWS cadastral geodatabase into two \code{\link[sf]{sf}} and
+#'  stores them for faster loading on subsequent use by
+#'  \code{link{prep_properties}}. Exported but not typically called
+#'  directly by user unless \code{\link{install_fws_cadastral}} fails.
 #'
-#' @param prop character string; USFWS properties from which to extract
-#'  species occurrence records.  See \code{\link{fw_spp}}.
-#' @param bnd character scalar; which property boundary to use.  See
-#'  \code{\link{fw_spp}}
-#' @param verbose logical; suppress messaging? See \code{\link{fw_spp}}
-#'  during species occurrence queries
 #' @export
-prep_cadastral <- function(prop, bnd, verbose) {
+prep_cadastral <- function() {
+
+  check_cadastral()
 
   # Get requested features
   gdb <- system.file("extdata", "FWSCadastral.gdb", package = "fwspp")
 
-  if (identical(bnd, "admin"))
-    r <- sf::read_sf(gdb, layer = "FWSInterest", stringsAsFactors = FALSE, quiet = TRUE)
-  else
-    r <- sf::read_sf(gdb, layer = "FWSApproved", stringsAsFactors = FALSE, quiet = TRUE)
+  message(wrap_message(
+    paste("Storing USFWS cadastral geodatabase in a more efficient format.",
+          "This will take several additional minutes.")))
+  for (l in c("FWSInterest", "FWSApproved")) {
+    message("Processing ", l)
+    props <- sf::read_sf(gdb, layer = l, stringsAsFactors = FALSE, quiet = TRUE)
+    l_nm <- paste0("fws_", tolower(sub("FWS", "", l)))
 
-  # Accommondate inconsistency with D'Arbonne in Approved vs Interest
-  r <- mutate(r,
-              ORGNAME = gsub(" '", "'", .data$ORGNAME),
-              ORGNAME = gsub("([A-Z])(\\.)([A-Z])", "\\1\\2 \\3", .data$ORGNAME))
+    # Accommondate inconsistency with D'Arbonne in Approved vs Interest
+    props <- mutate(props,
+                    ORGNAME = gsub(" '", "'", .data$ORGNAME),
+                    ORGNAME = gsub("([A-Z])(\\.)([A-Z])", "\\1\\2 \\3", .data$ORGNAME))
 
-  # Check for MULTISURFACE geometry and cast to MULTIPOLYGON
-  if ("MULTISURFACE" %in% unique(st_geometry_type(st_geometry(r))))
-    r <- sf::st_cast(r, "MULTIPOLYGON")
+    # Cast to MULTIPOLYGON to avoide issues with MULTISURFACE geometries
+    props <- sf::st_cast(props, to = "MULTIPOLYGON")
 
-  # Reduce to properties matching input query
-  props <- filter(r, .data$ORGNAME %in% prop) %>%
-    select(.data$ORGNAME, .data$FWSREGION)
-  prop_labels <- props$ORGNAME %>% unique() %>% Cap() %>% shorten_orgnames()
+    # Impose zero width buffer to correct potentially invalid geometries
+    # e.g., ring self-intersections...
+    props <- suppressWarnings(sf::st_buffer(props, 0))
 
-  # Dissolve into a single multi-part polygon by property
-  if (!identical(nrow(props), length(prop_labels))) {
+    # Dissolve into a single multi-part polygon by property
+    # Include ORGCODE as well to overcome identically-names properties
     props <- props %>%
-      group_by(.data$ORGNAME) %>%
-      summarise()
+      select(.data$ORGNAME, .data$ORGCODE, .data$FWSREGION) %>%
+      group_by(.data$ORGNAME, .data$ORGCODE) %>%
+      summarize()
+
+    # Put in WGS84 even though GRS80 is practically identical
+    props <- sf::st_transform(props, 4326)
+
+    out_fn <- file.path(system.file("extdata", package = "fwspp"),
+                        paste0(l_nm, ".rds"))
+    saveRDS(props, file = out_fn)
   }
-
-  # Impose zero width buffer to correct potentially invalid geometries
-  # e.g., ring self-intersections...
-  props <- suppressWarnings(sf::st_buffer(props, 0))
-
-  # Put in WGS84 even though GRS80 is practically identical
-  props <- sf::st_transform(props, 4326)
-
-  # Display queried properties
-  if (verbose) {
-    message(paste(c(paste0(length(prop_labels), " properties will be queried:"),
-                    strwrap(paste(sort(prop_labels), collapse = ", "),
-                            indent = 4, exdent = 4)),
-                  collapse = "\n"))
-  }
-  props
+  message("Finished!")
 }
