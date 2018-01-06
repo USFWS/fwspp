bccd10801e4c7cf9ecd6e3e75f247004051eee29
Adam Smith <avianmigration@gmail.com>
2018-01-06 18:11:10 +0000

    More appropriate fxn naming

--- R/fwspp_combine.R
+++ R/fwspp_combine.R
@@ -16,35 +16,35 @@
 #' ml <- fws_occ(fws = ml)
 #' mb <- find_fws("mountain bog")
 #' mb <- fws_occ(fws = mb)
-#' mlb <- combine_occ(ml, mb)
+#' mlb <- fwspp_combine(ml, mb)
 #' }
 
-combine_occ <- function(...) {
-  occ_list <- list(...)
+fwspp_combine <- function(...) {
+  fwspp_list <- list(...)
 
   # Validating that objects can be combined
-  if (length(occ_list) < 2) stop("Need at least two `fwspp` objects to use this function.")
-  if (!all(sapply(occ_list, inherits, "fwspp")))
+  if (length(fwspp_list) < 2) stop("Need at least two `fwspp` objects to use this function.")
+  if (!all(sapply(fwspp_list, inherits, "fwspp")))
     stop("All inputs must be `fwspp` objects.")
   exp_attr <- c("names", "class", "boundary", "scrubbing", "buffer_km", "query_dt")
-  if (!all(sapply(occ_list, function(x) identical(names(attributes(x)), exp_attr))))
+  if (!all(sapply(fwspp_list, function(x) identical(names(attributes(x)), exp_attr))))
     stop("One or more `fwspp` objects is missing expected attributes.")
-  if (length(unique(sapply(occ_list, has_taxonomy))) > 1)
+  if (length(unique(sapply(fwspp_list, has_taxonomy))) > 1)
     stop("One of these things is not like the others...\n",
          "    Input objects must all lack (or all possess) taxonomic information.")
   core_attr <- exp_attr[2:5]
-  core_attr <- lapply(occ_list, function(x) attributes(x)[core_attr])
+  core_attr <- lapply(fwspp_list, function(x) attributes(x)[core_attr])
   if (length(unique(core_attr)) != 1)
     stop("Cannot combine `fwspp` objects with different `boundary`, `scrubbing`, ",
          "or `buffer` settings.")
 
   # Combining
-  orgnames <- unlist(lapply(occ_list, function(x) attributes(x)[["names"]]))
-  query_dts <- lapply(occ_list, function(x) attributes(x)[["query_dt"]])
+  orgnames <- unlist(lapply(fwspp_list, function(x) attributes(x)[["names"]]))
+  query_dts <- lapply(fwspp_list, function(x) attributes(x)[["query_dt"]])
   earliest_query <- query_dts[[which.min(query_dts)]]
-  out_occ <- do.call("c", occ_list)
-  attributes(out_occ) <- c(list(names = orgnames),
+  out_fwspp <- do.call("c", fwspp_list)
+  attributes(out_fwspp) <- c(list(names = orgnames),
                            core_attr[[1]],
                            list(query_dt = earliest_query))
-  out_occ
+  out_fwspp
 }
