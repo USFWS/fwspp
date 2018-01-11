9e3713964f893d9c18f2ab9f506bfbcd107e0897
Adam Smith <avianmigration@gmail.com>
2018-01-11 02:26:03 +0000

    Split Excel constructor fxns and rename

---
+++ R/review_helpers.R
@@ -0,0 +1,80 @@
+add_review_validation <- function(wb, end_row) {
+  openxlsx::dataValidation(wb, 1, col = 6, rows = 2:end_row,
+                 type = "list", value = "'tags'!$A$1:$A$5")
+  openxlsx::dataValidation(wb, 1, col = 7, rows = 2:end_row,
+                 type = "list", value = "'tags'!$B$1:$B$8")
+  openxlsx::dataValidation(wb, 1, col = 8, rows = 2:end_row,
+                 type = "list", value = "'tags'!$C$1:$C$3")
+}
+
+is_review <- function(df) {
+  rev_cols <- c("org_name", "category", "taxon_code", "sci_name",
+                "com_name", "occurrence", "nativeness", "accept_record",
+                "evidence", "note")
+  identical(names(df), rev_cols)
+}
+
+import_review <- function(xlsx, verbose) {
+  review <- try(
+    readxl::read_excel(xlsx,
+                       col_types = c("text", "text", "numeric", "text",
+                                     "text", "text", "text", "text",
+                                     "text", "text")), silent = TRUE)
+  if (is_error(review)) {
+    warning(basename(xlsx), " does not match expected format. Skipping.", call. = FALSE)
+    return()
+  }
+  if (!is_review(review)) {
+    warning(basename(xlsx), " does not match expected format. Skipping.", call. = FALSE)
+    return()
+  }
+  if (verbose) cat(basename(xlsx), "imported successfully.\n")
+  review
+}
+
+process_review <- function(df) {
+  # Remove unaccepted observations
+  df <- filter(df, .data$accept_record != "No")
+
+  # Pull modified records
+  acc_recs <- filter(df, .data$accept_record == "Yes")
+  mods <- filter(df, .data$accept_record == "Modify" & !is.na(.data$taxon_code))
+
+  revised_codes <- unique(mods$taxon_code)
+  message("Retrieving taxonomy for modified records.")
+  revised_codes <- pbapply::pblapply(revised_codes, nps_taxonomy_by_code) %>%
+    bind_rows() %>%
+    mutate(acc_sci_name = .data$sci_name) %>%
+    select(.data$taxon_code, .data$category, .data$acc_sci_name, .data$com_name)
+
+  # Join updated taxonomy to modified records
+  mods <- select(mods, -.data$com_name, -.data$category) %>%
+    left_join(revised_codes, by = "taxon_code") %>%
+    mutate(sci_name = ifelse(is.na(.data$acc_sci_name),
+                             .data$sci_name, .data$acc_sci_name))
+  acc_recs <- bind_rows(acc_recs, mods)
+
+  # Add cost center
+  cost_centers <- get_unit_codes()
+  acc_recs <- left_join(acc_recs, cost_centers, by = "org_name")
+
+  # Rename relevant columns
+  acc_recs <- acc_recs %>%
+    select(`Scientific Name` = .data$sci_name, TaxonCode = .data$taxon_code,
+           .data$UnitCode, CommonNames = .data$com_name,
+           ExternalLinks = .data$evidence, Occurrence = .data$occurrence,
+           Nativeness = .data$nativeness, ORGNAME = .data$org_name) %>%
+    mutate(RecordStatus = "Approved",
+           RefugeAccepted = "Yes")
+
+  ## Set column names/order of output data frame
+  out_df <- utils::read.csv(
+    text = paste(c("Scientific Name", "TaxonCode", "ORGNAME", "UnitCode", "CommonNames",
+                   "Refuge Synonyms", "ExternalLinks", "Occurrence", "OccurrenceClass",
+                   "Nativeness", "Management", "Abundance", "AbundanceNotes", "SpringAbundance",
+                   "SummerAbundance", "FallAbundance", "WinterAbundance", "RecordStatus",
+                   "RefugeAccepted", "Sensitive", "SensitiveNotes", "HistoryNotes"),
+                 collapse = ", "), check.names = FALSE)
+  out_df <- bind_rows(out_df, acc_recs)
+  out_df
+}
--- R/xlsx_constructors.R
+++ R/xlsx_constructors.R
@@ -1,9 +1,8 @@
-construct_review <- function(org, fwspp, overwrite, verbose, out_dir) {
+xlsx_review <- function(org, fwspp, overwrite, verbose, out_dir) {
   org_dat <- fwspp[[org]] %>%
     mutate(occurrence = "Probably present",
            nativeness = NA_character_,
-           accept_record = ifelse(is.na(.data$accept_record) |
-                                    grepl("NPSpecies", .data$accept_record),
+           accept_record = ifelse(is.na(.data$note) | grepl("NPSpecies", .data$note),
                                   "Yes", "No")) %>%
     select(.data$org_name, .data$category, .data$taxon_code,
            .data$sci_name, .data$com_name,
@@ -12,18 +11,18 @@ construct_review <- function(org, fwspp, overwrite, verbose, out_dir) {
     arrange(.data$category, .data$sci_name)
 
   wb <- createWorkbook()
-  addWorksheet(wb, "fwspp_review")
+  addWorksheet(wb, "Species List for Review")
   col_widths <- c(10, 15, 13, 29, 29, 16, 12, 9, 70, 40)
   setColWidths(wb, 1, cols = seq_along(org_dat), widths = col_widths)
   freezePane(wb, 1, firstRow = TRUE)
 
   # Add tags and data validation
-  create_tag_ws(wb)
-  add_validation(wb, end_row = nrow(org_dat) + 1)
+  xlsx_review_tags(wb)
+  add_review_validation(wb, end_row = nrow(org_dat) + 1)
 
   # Write and save it
   writeData(wb, 1, org_dat, withFilter = TRUE)
-  fn <- file.path(out_dir, construct_fn(org))
+  fn <- file.path(out_dir, xlsx_fn(org))
   if (file.exists(fn) && !overwrite) {
     warning("File exists and overwrite = FALSE. Skipping ", org, call. = FALSE)
   } else {
@@ -33,7 +32,28 @@ construct_review <- function(org, fwspp, overwrite, verbose, out_dir) {
   return()
 }
 
-create_tag_ws <- function(wb) {
+xlsx_submission <- function(org, occ_data, out_dir, overwrite, verbose) {
+  org_dat <- filter(occ_data, .data$ORGNAME == org) %>%
+    select(-.data$ORGNAME)
+
+  wb <- createWorkbook()
+  addWorksheet(wb, "Species List for Import")
+  setColWidths(wb, 1, cols = seq_along(org_dat), widths = "auto")
+  freezePane(wb, 1, firstRow = TRUE)
+
+  # Write and save it
+  writeData(wb, 1, org_dat, withFilter = TRUE)
+  fn <- file.path(out_dir, xlsx_fn(org))
+  if (file.exists(fn) && !overwrite) {
+    warning("File exists and overwrite = FALSE. Skipping ", org, call. = FALSE)
+  } else {
+    saveWorkbook(wb, fn, overwrite = overwrite)
+    if (verbose) cat("Exported", paste0(org, "\n"))
+  }
+  return()
+}
+
+xlsx_review_tags <- function(wb) {
   addWorksheet(wb, "tags")
   occurrence <- c("Present", "Present-Adjacent", "Probably Present",
                   "Probably Present-Adjacent",
@@ -47,16 +67,7 @@ create_tag_ws <- function(wb) {
   writeData(wb, 2, accept_record, startCol = 3)
 }
 
-add_validation <- function(wb, end_row) {
-  dataValidation(wb, 1, col = 6, rows = 2:end_row,
-                 type = "list", value = "'tags'!$A$1:$A$5")
-  dataValidation(wb, 1, col = 7, rows = 2:end_row,
-                 type = "list", value = "'tags'!$B$1:$B$8")
-  dataValidation(wb, 1, col = 8, rows = 2:end_row,
-                 type = "list", value = "'tags'!$C$1:$C$3")
-}
-
-construct_fn <- function(x) {
+xlsx_fn <- function(x) {
   x %>% gsub(" ", "_", .) %>%
     gsub("\\.|,|;", "", .) %>%
     paste0(".xlsx")
