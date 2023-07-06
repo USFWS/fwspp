2c80b9b86c5b515ef1b13d3f0af9ce1c35e30518
Fox <timothy_fox@fws.gov>
2023-07-06 16:06:24 -0400

    call the stringr package for the str_replace function

--- R/xlsx_constructors.R
+++ R/xlsx_constructors.R
@@ -172,7 +172,7 @@ xlsx_submission <- function(org, occ_data, out_dir, overwrite, verbose) {
         rawToChar(
           httr::GET(
             paste0("https://ecos.fws.gov/ServCatServices/v2/rest/taxonomy/searchByScientificName/",
-                   FWSpecies_df$scientificName[i] %>% str_extract( "[^ ]+ [^ ]+") %>% str_replace( " ", "%20")),httr::timeout(50000))$content)))
+                   FWSpecies_df$scientificName[i] %>% str_extract( "[^ ]+ [^ ]+") %>% stringr::str_replace( " ", "%20")),httr::timeout(50000))$content)))
     taxoncode_vec[i]<-ifelse(nrow(subset(test, toupper(test$ScientificName)==toupper(FWSpecies_df$scientificName[i])))==0,
                              "<null>",max(subset(test, toupper(test$ScientificName)==toupper(FWSpecies_df$scientificName[i]))$TaxonCode))
     rm(test)
